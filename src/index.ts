ee32ebd4a487cecce7f80e5afee7db7cbf5075a7
hemni28 <76481884+hemni28@users.noreply.github.com>
2021-03-22 23:41:25 +0900

    study

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,47 @@
+// // const name = "Hailey",
+// //     age = 30,
+// //     gender = "female";
+// // interface Human {
+// //     name: string;
+// //     age: number;
+// //     gender: string;
+// // }
+// //js에 인터페이스 컴파일 되지 않아서 class로 컨트롤 가능함 (단, ts에서는 클래스가 어떤 속성+권한들을 가지는지 선언해줘야함)
+// class Human {
+//     public name: string;
+//     private age: number;
+//     public gender: string;
+//     constructor(name: string, age: number, gender?: string) { // 클래스가 시작될때 호출되는 method
+//         this.name = name;
+//         this.age = age;
+//         this.gender = gender;
+//     }
+// }
+// // const person = {
+// //     name: "Hey",
+// //     age: 20,
+// //     gender: "male"
+// // }
+// const lynn = new Human("Lynn", 18);
+//
+// // 각 파라미터에 type을 지정해줄 수 있음, return 형태도 지정해줌
+// const sayHi = (name:string, age:number, gender?:string): void => {
+//     console.log(`Hi ${name}, you are ${age}, you are a ${gender}`);
+// }
+// // return 형태를 string으로 지
+// const sayHello = (person: Human): string => {
+//     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+// }
+// // arguments를 2개만 적으면 에러를 내서, 컴파일도 안되기 때문에 실수 막아줌
+// // 그러나 파라미터에 ?를 붙여주면 optional로 인식해서 에러나지 않음 wow!
+// sayHi("Hailey", 30, "female");
+// // console.log(sayHello(person));
+// console.log(sayHello(lynn));
+
 import * as CryptoJS from "crypto-js";
 
 class Block {
+  // Static method > method가 Block안에 있고, 클래스가 생성되지 않아도 호출할수 있는 Method다
   static calculateBlockHash = (
     index: number,
     previousHash: string,
@@ -39,7 +80,7 @@ class Block {
 
 const genesisBlock: Block = new Block(0, "2020202020202", "", "Hello", 123456);
 
-let blockchain: Block[] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];//Block의 array! Block이 아니면 에러!!
 
 const getBlockchain = (): Block[] => blockchain;
 
@@ -102,4 +143,5 @@ createNewBlock("fourth block");
 
 console.log(blockchain);
 
+// 모듈이 된다고 선언해줘야함 (bug인듯?)
 export {};
